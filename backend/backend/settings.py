ca30ccc34a3c9b08e8922e270e24a15fd89fe1f1
Krabs <vovastr0v@yandex.ru>
2024-05-29 18:12:35 +0500

    222

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -192,11 +192,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
-
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = "/var/www/backendapp2412.com/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
