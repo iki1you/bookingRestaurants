f158baa1065e98119ae38f638770789975be18ad
Krabs <vovastr0v@yandex.ru>
2024-05-30 19:51:25 +0500

    222

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -174,7 +174,7 @@ PASSWORD_HASHERS = [
 #    'https://booking-restaurants.vercel.app',
 #]
 
-#CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = False
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
