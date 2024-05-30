b1f73067defe126cb58fa9a11ee732edd108e95d
Krabs <vovastr0v@yandex.ru>
2024-05-31 00:31:36 +0500

    222

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -203,8 +203,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'media_root')
-MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'media_root')
+#MEDIA_URL = '/media/'
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
