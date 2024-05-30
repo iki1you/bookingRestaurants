d2fe7c6ad084daf32394fe60e5afa9aa57ca5765
Krabs <vovastr0v@yandex.ru>
2024-05-30 22:46:10 +0500

    222

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -70,4 +70,6 @@ urlpatterns = [
                   path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
                   path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
                   path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
